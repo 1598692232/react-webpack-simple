841ddde9c5f613878ab3ad7f4e9f9bb309b85e54
1037359589 <1037359589@qq.com>
2017-08-30 14:31:29 +0800

    tijiao

---
+++ app/main.js
@@ -0,0 +1,4 @@
+var React = require('react');
+var react_dom = require('react-dom');
+var AppComponent = require('./components/productBox.jsx');
+react_dom.render(<AppComponent />, document.getElementById('content'));
